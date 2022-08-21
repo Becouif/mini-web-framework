81f1acf30939d89b2e67c2cce17819fa8c638f04
becouif <lovguya@gmail.com>
2022-08-21 19:56:13 +0300

    using composition

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,16 @@
 import axios, { AxiosResponse } from 'axios';
+
 interface userProps {
   id: number;
   name: string;
   age: number;
 }
 // how to declare a callback as function
-type Callback = () => void;
 
 // start of class User
 export class User {
   // declare an obj without knowing what key it will have
-  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: userProps) {}
 
   get(propName: string): string | number {
@@ -21,20 +21,24 @@ export class User {
   }
 
   // this on method means we are going to call on with string as first argument and function as second argument
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-    handlers.forEach((callback) => {
-      callback();
-    });
-  }
+
+  // change to composition
+  //  *****
+  // on(eventName: string, callback: Callback): void {
+  //   const handlers = this.events[eventName] || [];
+  //   handlers.push(callback);
+  //   this.events[eventName] = handlers;
+  // }
+  // trigger(eventName: string): void {
+  //   const handlers = this.events[eventName];
+  //   if (!handlers || handlers.length === 0) {
+  //     return;
+  //   }
+  //   handlers.forEach((callback) => {
+  //     callback();
+  //   });
+  // }
+
   fetch(): void {
     axios
       .get(`http://localhost:3000/users/${this.get('id')}`)
