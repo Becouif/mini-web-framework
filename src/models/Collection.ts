53ca5c039664a4fa03ed8caff43f56eb436f7393
becouif <lovguya@gmail.com>
2022-08-27 01:03:08 +0300

    updated

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,13 +1,24 @@
 import { Eventing } from './Eventing';
+import axios, { AxiosResponse } from 'axios';
 import { User } from './User';
+import { UserProps } from './User';
 export class Collection {
   models: User[] = [];
   events: Eventing = new Eventing();
+  constructor(public rootUrl: string) {}
 
   get on() {
     return this.events.on;
   }
   get trigger() {
     return this.events.trigger;
+  }
+  fetch(): void {
+    axios.get(this.rootUrl).then((response: AxiosResponse) => {
+      response.data.forEach((value: UserProps) => {
+        const user = User.buildUser(value);
+        this.models.push(user);
+      });
+    });
   }
 }
